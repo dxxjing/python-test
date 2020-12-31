fe3dde3d5a9057622e7a287e44bf50e3faf45cca
jingdx <1131807740@qq.com>
2020-12-31 18:24:54 +0800

    http get

--- base_program/http_get.py
+++ base_program/http_get.py
@@ -5,17 +5,23 @@ from urllib.request import urlopen
 import json
 
 
+# http get请求
+
 def get():
-    headers = {'Content-Type': 'application/json',
-               'HC-ACCESS-TOKEN': 'czo1OToiZTE0ZWFFWmZpSkNrd3Y5enNYTFRoLzJ6K0paUnZuT0ViR2dvTS9XLzNjSG5LZk9jL0wvY3YzR01XNHMiOw=='}
+    headers = {
+        'Content-Type': 'application/json',
+        'HC-ACCESS-TOKEN': 'czo1OToiZTE0ZWFFWmZpSkNrd3Y5enNYTFRoLzJ6K0paUnZuT0ViR2dvTS9XLzNjSG5LZk9jL0wvY3YzR01XNHMiOw=='
+    }
 
     url = 'http://test-hantalk-app.hanmaker.com/api/office/chat-group-list'
+
     with urlopen(Request(url, None, headers)) as res:
         res = res.read().decode()
 
     res = json.loads(res)
     print(res['data'][0]['group_name'])
 
-    #print(res)
+    # print(res)
+
 
 get()
